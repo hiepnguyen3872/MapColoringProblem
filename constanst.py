9b3f0498c1e275ce9f1f043e6600182b44f07aae
ppthqnhqp <76690542+ppthqnhqp@users.noreply.github.com>
2021-10-31 14:44:44 +0700

    Add files via upload

---
+++ constanst.py
@@ -0,0 +1,16 @@
+FPS = 30 # frames per second to update the screen
+WINWIDTH = 1000 # width of the program's window, in pixels
+WINHEIGHT = 650 # height in pixels
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+GREY = (  57, 57, 57)
+WHITE = (255, 255, 255)
+BLUE = (135,206,235)
+BGCOLOR = BLUE
+TEXTCOLOR = WHITE
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
